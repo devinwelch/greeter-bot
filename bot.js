3ebdbd30565527d1df7877356d52478386ba63c4
devinwelch <devinmwelch@gmail.com>
2017-12-07 14:58:34 -0500

    sassy bot

--- bot.js
+++ bot.js
@@ -20,6 +20,18 @@ client.on('message', message => {
         newMessage = message.content.substr(0, message.content.length - 3) + 'o' + message.content.substr(message.content.length - 2, message.content.length + 1);
         message.reply(newMessage);
     }
+    else if (Math.floor(Math.random() * 20) === 0) {
+        //random chance to get mocked
+        toggle = true;
+        mock = "";
+        for(i = 0; i < message.content.length; i++) {
+            mock += toggle ? message.content[i].toUpperCase() : message.content[i].toLowerCase();
+            if (message.content[i].match(/[a-z]/i)) {
+                toggle = !toggle;
+            }
+        }
+        message.channel.send(mock);
+    }
 });
 
 client.login(process.env.BOT_TOKEN);
