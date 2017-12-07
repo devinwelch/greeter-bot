e177ddebf756e8c5d02f8fa9db37a4d4a346824c
devinwelch <devinmwelch@gmail.com>
2017-12-06 23:46:17 -0500

    memes

--- bot.js
+++ bot.js
@@ -6,7 +6,17 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content.endsWith('ing') && message.content.match(/^[A-Za-z]+$/)) {
+    if (message.author.bot) {
+        return;
+    }
+    else if (message.content === "ting") {
+        message.channel.post("SKKKKRRRA");
+        message.channel.post("POP POP KOT KOT KOT");
+        message.channel.post("SKIBIKI POP POP... AND A PU PU PUDRRR BOOM");
+        message.channel.post("SKYA, DU DU KU KU DUN DUN");
+        message.channel.post("*POOM* *POOM*");
+    }
+    else if (message.content.endsWith('ing') && message.content.match(/^[A-Za-z]+$/)) {
         newMessage = message.content.substr(0, message.content.length - 3) + 'o' + message.content.substr(message.content.length - 2, message.content.length + 1);
         message.reply(newMessage);
     }
