cfc262c9dc7ee49d34c0fe6de0147a1d660522f2
devinwelch <devinmwelch@gmail.com>
2017-12-16 17:40:04 -0500

    Testing

--- bot.js
+++ bot.js
@@ -9,31 +9,38 @@ client.on('message', message => {
     if (message.author.bot) {
         return;
     }
+
     /*else if (message.content === message.channel.around.find(x => x.content === message.content)) {
         message.reply("dorse");
     }*/
+
+    //Play the best song ever
     else if (message.content === "!exposed")
     {
         var voiceChannel = message.member.voiceChannel;
         voiceChannel.join().then(connection => {
-            const dispatcher = connection.playFile('./Sounds/Exposed.mp3');
+            const dispatcher = connection.playFile('./Doable.mp3');
             dispatcher.on("end", end => {
                 voiceChannel.leave();
             });
         }).catch(error => console.log(error));
     }
+
+    //Enforce some positivity
     else if (isQuestion(message.content))
     {
         message.channel.send("test");
         var voiceChannel = message.member.voiceChannel;
         voiceChannel.join().then(connection => {
-            const dispatcher = connection.playFile('./Sounds/Doable.mp3');
+            const dispatcher = connection.playFile('./Doable.mp3');
             dispatcher.on("end", end => {
                 voiceChannel.leave();
             });
         }).catch(error => console.log(error));
         message.react(thatsdoable.id);
     }
+
+    //Man's not hot
     else if (message.content === "ting") {
         message.channel.send("SKKKKRRRA");
         message.channel.send("POP POP KOT KOT KOT");
@@ -41,12 +48,15 @@ client.on('message', message => {
         message.channel.send("SKYA, DU DU KU KU DUN DUN");
         message.channel.send("*POOM* *POOM*");
     }
+
+    //Ping pong!
     else if (message.content.endsWith('ing') && message.content.match(/^[A-Za-z]+$/)) {
         newMessage = message.content.substr(0, message.content.length - 3) + 'o' + message.content.substr(message.content.length - 2, message.content.length + 1);
         message.reply(newMessage);
     }
+
+    //Random chance to make fun of you
     else if (Math.floor(Math.random() * 20) === 0) {
-        //random chance to get mocked
         toggle = true;
         mock = "";
         for(i = 0; i < message.content.length; i++) {
