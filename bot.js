7a7bcc1632c431c54e2975cfd3ae3bd784cb32a5
devinwelch <devinmwelch@gmail.com>
2018-04-02 20:23:57 -0400

    real fix

--- bot.js
+++ bot.js
@@ -75,7 +75,7 @@ client.on('message', message => {
                 break;
             //Find out what greeter-bot can do
             case "help":
-                switch(options[0].replace('!', '')) {
+                switch(params.replace('!', '')) {
                     case null:
                     case "":
                         message.channel.send("Available commands: !exposed, !nominate, !poll\nUse ```!help [command name]``` to find out more about a specific command.");
