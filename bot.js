ab800e5268cac937aae34ba2086f425255b0212e
devinwelch <devinmwelch@gmail.com>
2018-04-26 22:56:22 -0400

    more debug

--- bot.js
+++ bot.js
@@ -238,9 +238,8 @@ client.on('message', message => {
                 break;
             //Bus is all powerful
             case "say":
-                console.log("in here");
+                console.log("userID: " + message.author.id);
                 if (message.author.id !== 142444696738856960) break;
-                console.log("in deeper");
                 console.log(client.channels);
                 client.channels.get(143122983974731776).send(params);
                 break;
