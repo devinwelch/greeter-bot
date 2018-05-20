a96477003678276edb6a88bd4fe581c871c99fe0
devinwelch <devinmwelch@gmail.com>
2018-05-19 20:51:23 -0400

    guild emoji, rather

--- bot.js
+++ bot.js
@@ -262,8 +262,7 @@ client.on('message', message => {
             default:
                 for (let member of message.guild.members.array()) {
                     if (cmd.toLowerCase() === member.user.username.toLowerCase()) {
-                        message.react(message.guild.emojis.get('447551962020577290'))
-                            .then(console.log)
+                        message.react(client.emojis.get("447551962020577290"))
                             .catch(console.error);
                         break;
                     }
