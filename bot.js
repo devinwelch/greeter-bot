020d446754c7cd2bd9287945326459f2be34f903
Devin <devinmwelch@gmail.com>
2020-03-11 20:44:41 -0400

    legacy code

--- bot.js
+++ bot.js
@@ -404,12 +404,12 @@ schedule.scheduleJob('0 * * * * *', function() {
                 role.id === '687436756559200367'))) {
                     let noninfected = voiceChannel.members.array().filter(member => member.roles.array().every(role =>
                         role.id !== '687436756559200367'))
-                    console.log('noninfected is ' + noninfected);
+                    //console.log('noninfected is ' + noninfected);
                     if (Math.floor(Math.random() * 1) === 0) {
                         r = Math.floor(Math.random() * noninfected.length)
-                        console.log('noninfected[r] is ' + noninfected[r]);
-                        console.log('noninfected[r].roles is ' + noninfected[r].roles);
-                        noninfected[r].roles.add('687436756559200367')
+                        //console.log('noninfected[r] is ' + noninfected[r]);
+                        //console.log('noninfected[r].roles is ' + noninfected[r].roles);
+                        noninfected[r].addRole('687436756559200367', 'he got infected!')
                         client.channels.get("466065580252725288").send(noninfected[r].user.username + ' caught the coronavirus! Oh no, stay away!')
                     }
                 }
