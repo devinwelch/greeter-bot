39d457958428a8b4cdf37cb4d93c42cf8c03e62a
Devin <devinmwelch@gmail.com>
2020-11-22 11:31:41 -0500

    error checking for sing

--- commands/sing.js
+++ commands/sing.js
@@ -12,8 +12,14 @@ self.execute = async function(client, config, db, message, args) {
     args = args.split(' ');
     playYouTube(client, message.member.voice.channel, args[0], { seek: args.length > 1 ? args[1] : 0, timeout: 10000 });
     
-    const info = await ytdl.getBasicInfo(args[0]);
-    message.delete()
-        .then(msg => console.log(`${msg.author.username} sang ${info.videoDetails.title} (${info.videoDetails.video_url})`))
-        .catch(console.error);
+    try {
+        const info = await ytdl.getBasicInfo(args[0]).catch(console.error);
+        message.delete()
+            .then(msg => console.log(`${msg.author.username} sang ${info.videoDetails.title} (${info.videoDetails.video_url})`))
+            .catch(console.error);
+    }
+    catch (error) {
+        message.delete().catch(console.error);
+        console.log(`Cannot get details for : ${args[0]}`);
+    }
 };
