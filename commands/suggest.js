267571020daf074c3d31d4a3d56ea28f74471339
Devin <devinmwelch@gmail.com>
2021-03-25 18:29:46 -0400

    suggestion box

---
+++ commands/suggest.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: 'begone',
+    description: 'Kick greeter-bot out of the voice channel.',
+    category: 'sound',
+    aliases: ['begonebot'],
+    execute(client, config, db, message, args) {
+        const channel = message.guild.me.voice.channel;
+        if (channel && !client.rocking) {   
+            channel.leave();
+        }
+    }
+};
