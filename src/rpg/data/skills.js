fb6b01b70b3c4d5f4154c80c67feab18057f0539
Devin <devinmwelch@gmail.com>
2022-10-08 15:26:41 -0400

    skills

--- src/rpg/data/skills.js
+++ src/rpg/data/skills.js
@@ -6,5 +6,14 @@ export default {
         upgrade: "Don't like fighting? Increase your odds when you gamble!",
         skillUpgrades: 5,
         icon: '🍀'
+    },
+
+    //Backpack
+    backpack: {
+        type: 'backpack',
+        name: 'Backpack',
+        upgrade: 'Increase maximum inventory size by 5!',
+        skillUpgrades: 3,
+        icon: '🎒'
     }
 };
